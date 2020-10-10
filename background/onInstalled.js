5a43ab28f516b42f0186d06942c73ba5c81d0e68
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2020-10-10 14:56:00 +0100

    Small fix

--- background/onInstalled.js
+++ background/onInstalled.js
@@ -2,7 +2,6 @@
 chrome.runtime.onInstalled.addListener((details) => {
 
     if (details.reason === "install") {
-        // If storage is empty, set it as empty array
         chromeAsync.storage.sync.set({
             [STORAGE_KEYS.WATCHING_TICKETS]: STORAGE_DEFAULTS[STORAGE_KEYS.WATCHING_TICKETS],
             [STORAGE_KEYS.NOTIFICATIONS]: STORAGE_DEFAULTS[STORAGE_KEYS.NOTIFICATIONS],
@@ -12,13 +11,8 @@ chrome.runtime.onInstalled.addListener((details) => {
             [STORAGE_KEYS.FILE_EXTENSIONS]: STORAGE_DEFAULTS[STORAGE_KEYS.FILE_EXTENSIONS]
         });
 
-        // If storage is empty, set it as empty array
-        chromeAsync.storage.local.get([STORAGE_KEYS.WATCHING_TICKETS]).then((objects) => {
-            if (!Array.isArray(objects[STORAGE_KEYS.WATCHING_TICKETS])) {
-                chromeAsync.storage.local.set({ [STORAGE_KEYS.WATCHING_TICKETS]: STORAGE_DEFAULTS[STORAGE_KEYS.WATCHING_TICKETS] });
-            }
-        });
+        chromeAsync.storage.local.set({ [STORAGE_KEYS.WATCHING_TICKETS]: STORAGE_DEFAULTS[STORAGE_KEYS.WATCHING_TICKETS] });
 
-        createAlarm(ALARMS.RELOAD_ALL_TICKETS, Date.now() + 2000, intervalReloadWatchingTickets || STORAGE_DEFAULTS.INTERVAL_RELOAD_WATCHING_TICKETS);
+        createAlarm(ALARMS.RELOAD_ALL_TICKETS, Date.now() + 2000, STORAGE_DEFAULTS[INTERVAL_RELOAD_WATCHING_TICKETS]);
     }
 });
